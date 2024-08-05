f576749190ff1802f3a63b015cd645539da1091a
dot-all <al.quirozsilva@gmail.com>
2024-08-05 17:04:24 -0400

    chore: add dark theme palette

--- client/src/books/pages/SearchPage.tsx
+++ client/src/books/pages/SearchPage.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import Navbar from '../../ui/Navbar';
 import { searchBooks } from '../../api/api';
-import { Book, IndustryIdentifier } from '../../types/book';
-import { Link } from 'react-router-dom';
+import { Book } from '../../types/book';
 import BookCard from '../components/BookCard';
+import Footer from '../../ui/Footer';
 
 export default function Search() {
   const [searchTerm, setSearchTerm] = useState<string>('');
@@ -32,33 +32,36 @@ export default function Search() {
   return (
     <>
       <Navbar />
-      <main className="w-11/12 mx-auto mt-8">
-        <h2 className="text-2xl font-medium mb-4 text-orange-500">Buscar Libro</h2>
-        <form
-          onSubmit={handleSearch}
-          className="flex flex-col md:flex-row md:w-1/2"
-        >
-          <input
-            type="text"
-            placeholder="Buscar por título, autor o ISBN"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
-            className="border border-gray-300 rounded-lg px-4 py-2 mb-4 md:mb-0 w-full mr-auto"
-          />
-          <button
-            type="submit"
-            className="bg-orange-500 text-white px-6 py-2 rounded-lg hover:bg-orange-400 transition-colors"
-          >
-            Buscar
-          </button>
-        </form>
-        {error && (
-          <p className="mt-8 text-red-500">{error}</p>
-        )}
-        {book && (
-          <BookCard book={book} />
-        )}
+      <main className="pt-8 dark:bg-[#202124] min-h-screen">
+        <section className='w-11/12 mx-auto'>
+          <h2 className="text-2xl font-medium mb-4 text-orange-500">Buscar Libro</h2>
+          <form
+            onSubmit={handleSearch}
+            className="flex flex-col md:flex-row md:w-1/2"
+            >
+            <input
+              type="text"
+              placeholder="Buscar por título, autor o ISBN"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+              className="border border-gray-300 rounded-lg px-4 py-2 mb-4 md:mb-0 w-full mr-auto dark:bg-[#28292a]"
+              />
+            <button
+              type="submit"
+              className="bg-orange-500 text-white px-6 py-2 rounded-lg hover:bg-orange-400 transition-colors"
+              >
+              Buscar
+            </button>
+          </form>
+          {error && (
+            <p className="mt-8 text-red-500">{error}</p>
+          )}
+          {book && (
+            <BookCard book={book} />
+          )}
+        </section>
       </main>
+      <Footer/>
     </>
   );
 }
