0e0ad3d1f5e9150a891ba8094dca631bfc2b85ae
dot-all <al.quirozsilva@gmail.com>
2024-08-05 17:02:06 -0400

    feat: add restructuring of carousels and skeleton

--- client/src/books/pages/HomePage.tsx
+++ client/src/books/pages/HomePage.tsx
@@ -1,60 +1,49 @@
-import { useEffect, useState } from 'react';
-
-interface Book {
-  id: string;
-  volumeInfo: {
-    title: string;
-    imageLinks?: {
-      thumbnail?: string;
-      smallThumbnail?: string;
-    };
-  };
-}
-
-interface BooksResponse {
-  items: Book[];
-}
+import { useBooks } from '../../contexts/BooksContext';
+import Navbar from '../../ui/Navbar';
+import Skeleton from 'react-loading-skeleton';
+import 'react-loading-skeleton/dist/skeleton.css';
+import { Carousel } from '../components/Carousel';
+import { Book } from '../../types/book';
+import Footer from '../../ui/Footer';
 
 export default function HomePage() {
-  const [data, setData] = useState<Book[] | null>(null);
-  const apiKey = import.meta.env.VITE_API_KEY as string;
-  const genre = "terror";
-  const endPoint = `https://www.googleapis.com/books/v1/volumes?q=subject:${genre}&maxResults=10&key=${apiKey}`;
+  const { horrorBooks, fictionBooks, romanceBooks } = useBooks();
 
-  async function fetchData() {
-    try {
-      const response = await fetch(endPoint);
-      const data: BooksResponse = await response.json();
-      console.log(data);
+  const skeletonSlides = Array.from({ length: 8 }).map((_, index) => (
+    <div key={index} className="flex flex-col items-center justify-center mx-2 my-1">
+      <div className="h-32 w-24 sm:h-40 sm:w-28 md:h-48 md:w-36 lg:h-56 lg:w-40">
+        <Skeleton className="h-full w-full" />
+      </div>
+      <div className="mt-2 w-24 sm:w-28 md:w-36 lg:w-40">
+        <Skeleton className="h-5 w-full" />
+      </div>
+    </div>
+  ));
 
-      setData(data.items);
-    } catch (e) {
-      console.error("Error:", e);
-    }
-  }
-
-  useEffect(() => {
-    fetchData();
-  }, []);
+  const renderCarousel = (title: string, data: Book[] | null, id: string) => (
+    <>
+      <h2 className='pb-4 pt-8 text-xl md:text-xl text-orange-500 font-medium'>{title}</h2>
+      {data ? (
+        <Carousel data={data} id={id} />
+      ) : (
+        <div className="swiper__container">
+          <div className="flex flex-row">{skeletonSlides}</div>
+        </div>
+      )}
+    </>
+  );
 
   return (
-    <div>
-      <h1>LIBRARY</h1>
-      {data ? (
-        data.map((book) => (
-          <div key={book.id}>
-            <p>{book.volumeInfo.title}</p>
-            {book.volumeInfo.imageLinks && (
-              <img
-                src={book.volumeInfo.imageLinks.thumbnail || book.volumeInfo.imageLinks.smallThumbnail}
-                alt={book.volumeInfo.title}
-              />
-            )}
-          </div>
-        ))
-      ) : (
-        <p>Loading...</p>
-      )}
-    </div>
+    <>
+      <Navbar />
+      <main className='dark:bg-[#202124]'>
+        <section className='w-11/12 mx-auto'>
+        {renderCarousel('Sumérgete en el Miedo', horrorBooks, 'horror_carousel')}
+        {renderCarousel('Historias que Desafían la Realidad', fictionBooks, 'fiction_carousel')}
+        {renderCarousel('Relatos Románticos', romanceBooks, 'romance_carousel')}
+        </section>
+      </main>
+      <Footer/>
+    </>
   );
 }
