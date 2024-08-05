4be232e253b0493f5d66cbd917b39c639567f24c
dot-all <al.quirozsilva@gmail.com>
2024-08-05 17:02:56 -0400

    feat: add book, search, category and category list routes

--- client/src/books/routes/BooksRoutes.tsx
+++ client/src/books/routes/BooksRoutes.tsx
@@ -1,12 +1,20 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import HomePage from "../pages/HomePage";
+import BookPage from "../pages/BookPage";
+import Search from "../pages/SearchPage";
+import CategoryPage from "../pages/CategoryPage";
+import ListCategoryPage from "../pages/ListCategoryPage";
 
 export default function BooksRoutes() {
   return (
     <>
     <Routes>
-      <Route path="app" element={<HomePage />} />
-      <Route path="/" element={<Navigate to="/app" />} />
+      <Route path="/" element={<HomePage />} />
+      <Route path="book/:id" element={<BookPage/>} />
+      <Route path="/*" element={<Navigate to="/" />} />
+      <Route path="/search" element={<Search/>} />
+      <Route path="/category" element={<CategoryPage/>} />
+      <Route path="/category/:category/list" element={<ListCategoryPage/>} />
     </Routes>
     </>
   )
