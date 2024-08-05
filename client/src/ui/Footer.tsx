2f9308796e4015e24d5d5fde661e02b5af0e5f33
dot-all <al.quirozsilva@gmail.com>
2024-08-05 17:03:47 -0400

    feat: add footer

---
+++ client/src/ui/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="h-40 dark:bg-[#202124] dark:text-white">
+      <section className="w-11/12 h-full mx-auto flex items-center justify-center">
+      <p>Creado por <a href="https://github.com/dot-all" target="_blank" className="text-orange-500">@dot-all</a></p>
+      </section>
+    </footer>
+  )
+}
