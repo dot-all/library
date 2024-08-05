791e054e6580de23ffb614381dd3ad4abadaa90b
dot-all <al.quirozsilva@gmail.com>
2024-08-04 22:46:39 -0400

    feat: add book provider

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import BookApp from './BookApp.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { BooksProvider } from './contexts/BooksContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <BookApp />
-    </BrowserRouter>
+    <BooksProvider>
+      <BrowserRouter>
+        <BookApp />
+      </BrowserRouter>
+    </BooksProvider>
   </React.StrictMode>,
 )
